a5f5a37bf8702d2d6389c2460f295a9db57ab58a
Minh Trường <letruong147123@gmail.com>
2023-01-07 21:23:21 +0700

    117-Maps-Fundamentals

---
+++ 09/117-Maps-Fundamentals/main.js
@@ -0,0 +1,29 @@
+"use strict";
+const rest = new Map();
+rest.set("name", "Classico Italian");
+rest.set(1, "Firenze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzaeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open ")
+  .set(false, "We are closed");
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, "test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));

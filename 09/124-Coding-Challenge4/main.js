c214f518977075ff30391db3f5dd74b85a20d508
Minh Trường <letruong147123@gmail.com>
2023-01-09 21:56:10 +0700

    124-Coding-Challenge4

---
+++ 09/124-Coding-Challenge4/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
+
+

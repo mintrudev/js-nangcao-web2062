f4c9d4922fd9cb989644735207f81d7914e348c5
Minh Trường <letruong147123@gmail.com>
2023-01-10 09:31:49 +0700

    128-Default-Parameters

---
+++ 10/128-Default-Parameters/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const bookings = [];
+const createBooking = (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) => {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+createBooking("LH123", undefined, 1000);

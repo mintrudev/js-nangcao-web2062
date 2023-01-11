65f23803660bd0773f7527d4b4d8273ff04aef56
Minh Trường <letruong147123@gmail.com>
2023-01-11 10:31:43 +0700

    130-First-Class-and-Higher-Order-Functions

---
+++ 10/130-First-Class-and-Higher-Order-Functions/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/*  Javascript treats functions as first-class citizens
+    This means that functions are simply values
+    functions are just another "type" of object
+*/
+/*  A functions receives another function as an argument, that returns a new function, or both
+    This is only possible because of first-class functions
+*/

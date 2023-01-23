fd0602833f10838a48549886de83f2303d0dc84d
Minh Trường <letruong147123@gmail.com>
2023-01-23 18:25:58 +0700

    189-Types-of-Events-and-Event-Handlers

---
+++ 13/189-Types-of-Events-and-Event-Handlers/main.js
@@ -0,0 +1,12 @@
+"use strict";
+const h1 = document.querySelector("h1");
+const alertH1 = (e) => {
+  alert("addEventListener: Great! You are reading the heading :D");
+};
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+
+// h1.onmouseenter = (e) => {
+//   alert("onmouseenter: Great! You are reading the heading :D");
+// };

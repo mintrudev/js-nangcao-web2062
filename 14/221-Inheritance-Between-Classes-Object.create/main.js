03a262391c109ff5315e74894a8e95a8ea03cc3e
Minh Trường <letruong147123@gmail.com>
2023-02-08 11:51:41 +0700

    221-Inheritance-Between-Classes-Object.create

---
+++ 14/221-Inheritance-Between-Classes-Object.create/main.js
@@ -0,0 +1,27 @@
+"use strict";
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce();
+jay.calcAge();

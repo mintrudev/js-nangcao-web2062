f3904fcab1e1c28467407c3b80beaa5cca5ad75d
Minh Trường <letruong147123@gmail.com>
2023-02-08 11:23:44 +0700

    220-Inheritance-Between-Classes-ES6-Classes

---
+++ 14/220-Inheritance-Between-Classes-ES6-Classes/main.js
@@ -0,0 +1,50 @@
+"use strict";
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet = function () {
+    console.log(`Hey ${this.fullName}`);
+  };
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  //  Set a property that already exists
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl("Martha Jones", 2012);
+martha.introduce();
+martha.calcAge();
